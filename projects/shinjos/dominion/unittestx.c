b0f381c15ba83a35a237a9554fcf8d69b3badd06
jbshin <josephshin93@gmail.com>
2019-02-07 01:29:29 -0500

    created template for individual unit and card tests

---
+++ projects/shinjos/dominion/unittestx.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+ *       Author: Joseph Shin
+ * Date Created: 2019 Feb 06
+ *  Description: Template for dominion code unit testing.
+ *****************************************************************************/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <stdio.h>
+#include <string.h>
+
+
+void unitTestX() {
+    // set up game states and return values
+
+    // modify one game state to expected game state
+
+    // run target function with original game state
+
+    // compare game states and return values
+
+    // interpret comparisons
+
+}
+
+int main() {
+    unitTestX();
+    return 0;
+}
