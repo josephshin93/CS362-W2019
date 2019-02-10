a34a57234d22ada413d29d046405317868b549e6
jbshin <josephshin93@gmail.com>
2019-02-10 16:06:02 -0500

    fixed some errors in scenario A, then added scenario B

--- projects/shinjos/dominion/cardtest2.c
+++ projects/shinjos/dominion/cardtest2.c
@@ -79,7 +79,7 @@ void testScenarioA(struct gameState *expc,
     expc->deck[currplayer][2] = -1;
     expc->deck[currplayer][1] = -1;
     expc->deckCount[currplayer] -= 3;
-    expc->playedCards[0] = adventurer;
+    expc->playedCards[0] = smithy;
     expc->playedCardCount++;
 
 
@@ -87,7 +87,7 @@ void testScenarioA(struct gameState *expc,
     ret = playCard(0, 0, 0, 0, resl);
 
 
-    printf("---------------------------------- Scenario X ----------------------------------\n");
+    printf("---------------------------------- Scenario A ----------------------------------\n");
 
     // compare return values
     printf("Return Value: ");
@@ -96,7 +96,7 @@ void testScenarioA(struct gameState *expc,
     else printf("%sWRONG%s   expected(%d) %c resulting(%d)\n", CRED, CNRM, expRet, (char)('='+compres), ret);
     
     // compare game states
-    compareStates(expc, resl, 1, 7);
+    compareStates(expc, resl, 1, 8);
 
     // compres = compareStatesAndSave(expc, resl, comp);
     // printf("compare game state return value: %d\n", compres);
@@ -108,6 +108,100 @@ void testScenarioA(struct gameState *expc,
     printf("--------------------------------------------------------------------------------\n");
 }
 
+/* Scenario B: Shuffle Scenario
+   2 players
+   empty suppy
+   no embargo tokens
+   no outpost played/turn
+   current player = 0
+   phase = action phase = 0
+   1 action
+   coins = 4 (copper, gold)
+   1 buy
+   current player hand = [smithy, copper, estate, gold, adventurer]
+   current player deck = [duchy, silver(top)]
+   current player discard = [estate, estate]
+   no played cards
+   empty next player hand/deck/discard
+ */
+void testScenarioB(struct gameState *expc, 
+                   struct gameState *resl, 
+                   struct gscomp *comp) {
+    int compres;
+    int ret, expRet = 0;
+    
+    // reset game states and game state comparisons
+    memset(expc, 0, sizeof(struct gameState));
+    memset(resl, 0, sizeof(struct gameState));
+    memset(comp, 0, sizeof(struct gscomp));
+
+    // declare scenario variables
+    int currplayer = 0;
+    
+    // set up scenario initial game state
+    resl->numPlayers = 2;
+    resl->whoseTurn = currplayer;
+    resl->phase = 0;
+    resl->numActions = 1;
+    resl->coins = 4; // copper, gold
+    resl->numBuys = 1;
+    resl->hand[currplayer][0] = smithy;
+    resl->hand[currplayer][1] = copper;
+    resl->hand[currplayer][2] = estate;
+    resl->hand[currplayer][3] = gold;
+    resl->hand[currplayer][4] = adventurer;
+    resl->handCount[currplayer] = 5;
+    resl->deck[currplayer][0] = duchy;
+    resl->deck[currplayer][1] = silver; // top
+    resl->deckCount[currplayer] = 2;
+    resl->discard[currplayer][0] = estate;
+    resl->discard[currplayer][1] = estate;
+    resl->discardCount[currplayer] = 2;
+
+    memcpy(expc, resl, sizeof(struct gameState));
+
+    // manually set scenario expected game state
+    expc->phase = 0; // not sure if this should change with playing a card
+    expc->numActions--;
+    expc->coins += 2; // + silver
+    expc->hand[currplayer][5] = silver;
+    expc->hand[currplayer][6] = adventurer;
+    expc->hand[currplayer][7] = estate;
+    expc->handCount[currplayer] += 3;
+    expc->deck[currplayer][1] = -1; // remove 2 for draw, add 2 from shuffle, remove 1 for draw
+    expc->deck[currplayer][0] = estate;
+    expc->deckCount[currplayer] -= 1;
+    expc->discard[currplayer][0] = -1; 
+    expc->discard[currplayer][1] = -1;
+    expc->discardCount[currplayer] -= 2; 
+    expc->playedCards[0] = smithy;
+    expc->playedCardCount++;
+
+
+    // call target function
+    ret = playCard(0, 0, 0, 0, resl);
+
+
+    printf("---------------------------------- Scenario B ----------------------------------\n");
+
+    // compare return values
+    printf("Return Value: ");
+    compres = (expRet > ret) - (expRet < ret);
+    if (compres == 0) printf("%sCORRECT%s\n", CGRN, CNRM);
+    else printf("%sWRONG%s   expected(%d) %c resulting(%d)\n", CRED, CNRM, expRet, (char)('='+compres), ret);
+    
+    // compare game states
+    compareStates(expc, resl, 1, 8);
+
+    // compres = compareStatesAndSave(expc, resl, comp);
+    // printf("compare game state return value: %d\n", compres);
+    // printGscomp(comp);
+
+
+    // interpret comparison(?)
+
+    printf("--------------------------------------------------------------------------------\n");
+}
 
 void cardTest2() {
     // declare game states and game state comparison
@@ -118,7 +212,7 @@ void cardTest2() {
     printf("target function: \n");
 
     testScenarioA(&expected, &resulting, &comp);
-    // testScenarioB(&expected, &resulting, &comp);
+    testScenarioB(&expected, &resulting, &comp);
 
     printf("================================================================================\n");
 }
